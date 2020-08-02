33658b34b45e534da9277927e3eabc7d489b82e2
jfkriz <jim@kriz.net>
2020-08-02 01:27:55 +0100

    Implement Javascript skeleton

---
+++ src/JavaScript/app.test.js
@@ -0,0 +1,7 @@
+const function1 = require('./app').default;
+const { doAllTheThings } = require('./app');
+
+// This is a Jest unit test - see https://jestjs.io/docs/en/getting-started for more information
+test('doAllTheThings should be true', () => {
+    expect(doAllTheThings()).toBeTruthy();
+});
