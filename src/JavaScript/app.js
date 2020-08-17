b80c1ea0ffefa7d72f3c18ac3bad51f8da2b5b86
jfkriz <jim@kriz.net>
2020-08-17 12:34:10 +0100

    initial implementation

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -1,8 +1,208 @@
 // Implement your exercise in this file.  If you need to implement additional functions,
 // remember to export them as well, if you need to access them in your unit test.
-function doAllTheThings() {
-    console.log("Hi there");
-    return true;
+const results = {
+    WIN: 'win',
+    LOSS: 'loss',
+    TIE: 'tie'
+};
+
+const ranks = {'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 10, 'Q': 11, 'K': 12, 'A': 13 };
+
+const suits = { 'S': 'Spades', 'H': 'Hearts', 'D': 'Diamonds', 'C': 'Clubs' };
+
+const outcomes = {
+    HIGH_CARD: 0,
+    PAIR: 1,
+    TWO_PAIR: 2,
+    THREE_OF_A_KIND: 3,
+    STRAIGHT: 4,
+    FLUSH: 5,
+    FULL_HOUSE: 6,
+    FOUR_OF_A_KIND: 7,
+    STRAIGHT_FLUSH: 8,
+    ROYAL_FLUSH: 9
+};
+
+class PokerHand {
+    constructor(cards) {
+        let arr = cards.split(' ');
+        this.cards = arr.map(card => new Card(card)).sort((a, b) => a.compareTo(b));
+    }
+
+    compareWith(other) {
+        let thisOutcome = this.determineOutcome();
+        let otherOutcome = other.determineOutcome();
+
+        if (thisOutcome.rank > otherOutcome.rank) {
+            return results.WIN;
+        }
+
+        if (thisOutcome.rank < otherOutcome.rank) {
+            return results.LOSS;
+        }
+
+        // Tiebreaker...
+        if (thisOutcome.tieBreaker > otherOutcome.tieBreaker) {
+            return results.WIN;
+        }
+
+        if (thisOutcome.tieBreaker < otherOutcome.tieBreaker) {
+            return results.LOSS;
+        }
+
+        return results.TIE;
+    }
+
+    determineOutcome() {
+        if (this.isRoyalFlush()) {
+            // TODO - any way to indicate tiebreaker failure?
+            return new Outcome(outcomes.ROYAL_FLUSH, 0);
+        }
+
+        if (this.isStraightFlush()) {
+            return new Outcome(outcomes.STRAIGHT_FLUSH, this.cards[4].rankValue());
+        }
+
+        if (this.isFourOfAKind()) {
+            let card = Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 4);
+            return new Outcome(outcomes.FOUR_OF_A_KIND, ranks[card[0]]);
+        }
+
+        if (this.isFullHouse()) {
+            let card = Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 3);
+            return new Outcome(outcomes.FULL_HOUSE, ranks[card[0]]);
+        }
+
+        if (this.isFlush()) {
+            return new Outcome(outcomes.FLUSH, this.cards[4].rankValue());
+        }
+
+        if (this.isStraight()) {
+            let highCard = this.cards[4];
+            if (highCard.rank == 'A' && this.cards[3].rank !== 'K') {
+                // If high card is an Ace, but next highest card is not a King, it is a straight with Ace low,
+                // so tiebreaker is the card at the top of the sequence (should be a 5)
+                highCard = this.cards[3];
+            }
+            return new Outcome(outcomes.STRAIGHT, highCard.rankValue());
+        }
+
+        if (this.isThreeOfAKind()) {
+            let card = Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 3);
+            return new Outcome(outcomes.THREE_OF_A_KIND, ranks[card[0]]);
+        }
+
+        if (this.isTwoPairs()) {
+            let tieBreaker = Object.entries(this.getUniqueRanks()).filter(entry => entry[1] == 2).map(entry => ranks[entry[0]]).sort()[1];
+            return new Outcome(outcomes.TWO_PAIR, tieBreaker);
+        }
+
+        if (this.isPair()) {
+            let card = Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 2);
+            return new Outcome(outcomes.PAIR, ranks[card[0]]);
+        }
+
+        return new Outcome(outcomes.HIGH_CARD, this.cards[4].rankValue());
+    }
+
+    isRoyalFlush() {
+        return this.isFlush() && this.cards[0].rank === 'T' && this.cards[4].rank === 'A';
+    }
+
+    isStraightFlush() {
+        return this.isFlush() && this.isStraight();
+    }
+
+    isFourOfAKind() {
+        return Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 4) !== undefined;
+    }
+
+    isFullHouse() {
+        let uniqueEntries = Object.entries(this.getUniqueRanks());
+        return uniqueEntries.length == 2 &&
+            uniqueEntries.find(entry => entry[1] == 3) !== undefined &&
+            uniqueEntries.find(entry => entry[1] == 2) !== undefined;
+    }
+
+    isFlush() {
+        return new Set(this.cards.map(card => card.suit)).size === 1;
+    }
+
+    isStraight() {
+        // If there's an Ace, this only accounts for Ace High - also need to account for Ace low
+        if (this.cards[4].rankValue() - this.cards[0].rankValue() == 4) {
+            return true;
+        }
+
+        // If no Ace, then no need to worry about Ace low
+        if (this.cards[4].rank !== 'A') {
+            return false;
+        }
+
+        // Ok, so high card is Ace - see if other 4 cards are 2 thru 5 (ranks will sum to 10)
+        return this.cards.slice(0, 4).map(card => card.rankValue()).reduce((acc, rank) => {
+            acc += rank;
+            return acc;
+        }, 0) == 10;
+    }
+
+    isThreeOfAKind() {
+        return Object.entries(this.getUniqueRanks()).find(entry => entry[1] === 3) !== undefined;
+    }
+
+    isTwoPairs() {
+        let uniqueEntries = Object.entries(this.getUniqueRanks());
+        return uniqueEntries.length == 3 &&
+            uniqueEntries.find(entry => entry[1] == 2).length === 2;
+    }
+
+    isPair() {
+        let uniqueEntries = Object.entries(this.getUniqueRanks());
+        return uniqueEntries.length == 4 &&
+            uniqueEntries.find(entry => entry[1] == 2) !== undefined;
+    }
+
+    getUniqueRanks() {
+        return this.cards.map(card => card.rank).reduce((acc, rank) => {
+            acc[rank] = acc[rank] === undefined ? 1 : acc[rank] + 1;
+            return acc;
+        }, {});
+    }
+
+    toString() {
+        return this.cards.map(card => card.toString()).join(', ');
+    }
 }
 
-module.exports = { doAllTheThings };
+class Card {
+    constructor(card) {
+        let arr = Array.from(card.toUpperCase());
+        this.rank = arr[0];
+        this.suit = arr[1];
+    }
+
+    suitName() {
+        return suits[this.suit];
+    }
+
+    rankValue() {
+        return ranks[this.rank];
+    }
+
+    compareTo(other) {
+        return this.rankValue() - other.rankValue();
+    }
+
+    toString() {
+        return `${this.rank} of ${suits[this.suit]}`;
+    }
+}
+
+class Outcome {
+    constructor(rank, tieBreaker) {
+        this.rank = rank;
+        this.tieBreaker = tieBreaker;
+    }
+}
+
+module.exports = { PokerHand, Card, outcomes, results };
--- src/JavaScript/app.test.js
+++ src/JavaScript/app.test.js
@@ -1,6 +1,165 @@
-const { doAllTheThings } = require('./app');
+const { PokerHand, outcomes, results } = require('./app');
 
 // This is a Jest unit test - see https://jestjs.io/docs/en/getting-started for more information
-test('doAllTheThings should be true', () => {
-    expect(doAllTheThings()).toBeTruthy();
+
+describe('PokerHand', () => {
+    describe('compareWith', () => {
+        describe('Royal Flush', () => {
+            test('beats Straight Flush', () => {
+                let hand1 = new PokerHand('AD TD KD JD QD');
+                let hand2 = new PokerHand('9D TD JD QD KD');
+    
+                expect(hand1.compareWith(hand2)).toBe(results.WIN);
+                expect(hand2.compareWith(hand1)).toBe(results.LOSS);
+            });
+        });
+
+        describe('Straight Flush', () => {
+            test('beats Four of a Kind', () => {
+                let hand1 = new PokerHand('AD 2D 3D 4D 5D');
+                let hand2 = new PokerHand('9D 9H 9S 9C AD');
+    
+                expect(hand1.compareWith(hand2)).toBe(results.WIN);
+                expect(hand2.compareWith(hand1)).toBe(results.LOSS);
+            });
+
+            test('high card breaks tie', () => {
+                let hand1 = new PokerHand('3D 4D 5D 6D 7D');
+                let hand2 = new PokerHand('2H 3H 4H 5H 6H');
+    
+                expect(hand1.compareWith(hand2)).toBe(results.WIN);
+                expect(hand2.compareWith(hand1)).toBe(results.LOSS);
+            });            
+        });
+
+        describe('Four of a Kind', () => {
+            test('beats Full House', () => {
+                let hand1 = new PokerHand('9D 9H 9S 9C AD');
+                let hand2 = new PokerHand('9D 9H 9S AC AD');
+    
+                expect(hand1.compareWith(hand2)).toBe(results.WIN);
+                expect(hand2.compareWith(hand1)).toBe(results.LOSS);
+            });
+
+            test('high card breaks tie', () => {
+                let hand1 = new PokerHand('TD TH TS TC AD');
+                let hand2 = new PokerHand('9D 9H 9S 9C AH');
+    
+                expect(hand1.compareWith(hand2)).toBe(results.WIN);
+                expect(hand2.compareWith(hand1)).toBe(results.LOSS);
+            });            
+        });
+    });
+
+    describe('determineOutcome', () => {
+        describe('Royal Flush', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('TD JD QD KD AD').determineOutcome().rank).toBe(outcomes.ROYAL_FLUSH)
+            });
+    
+            test('should not be Royal Flush if different suits', () => {
+                expect(new PokerHand('TD JS QD KD AD').determineOutcome().rank).not.toBe(outcomes.ROYAL_FLUSH)
+            });
+    
+            test('should not be Royal Flush if not Ten thru Ace', () => {
+                expect(new PokerHand('TD JD AS KD AD').determineOutcome().rank).not.toBe(outcomes.ROYAL_FLUSH)
+            });
+        });
+
+        describe('Straight Flush', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D TD JD QD KD').determineOutcome().rank).toBe(outcomes.STRAIGHT_FLUSH)
+            });
+    
+            test('Ace low should be correctly identified', () => {
+                expect(new PokerHand('AD 2D 3D 4D 5D').determineOutcome().rank).toBe(outcomes.STRAIGHT_FLUSH)
+            });
+
+            test('should not be Straight Flush if different suits', () => {
+                expect(new PokerHand('9D TS JD QD KD').determineOutcome().rank).not.toBe(outcomes.STRAIGHT_FLUSH)
+            });
+    
+            test('should not be Straight Flush if not 5 sequential cards', () => {
+                expect(new PokerHand('7D 9D TD JD QD').determineOutcome().rank).not.toBe(outcomes.STRAIGHT_FLUSH)
+            });
+        });
+
+        describe('Four of a Kind', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D 9H 9S 9C AS').determineOutcome().rank).toBe(outcomes.FOUR_OF_A_KIND)
+            });
+    
+            test('should not be identified if not correct cards', () => {
+                expect(new PokerHand('9D TS JD QD KD').determineOutcome().rank).not.toBe(outcomes.FOUR_OF_A_KIND)
+            });
+        });
+
+        describe('Full House', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D 9H 9S AC AS').determineOutcome().rank).toBe(outcomes.FULL_HOUSE)
+            });
+    
+            test('should not be identified if missing the pair', () => {
+                expect(new PokerHand('9D 9H 9S QD KD').determineOutcome().rank).not.toBe(outcomes.FULL_HOUSE)
+            });
+
+            test('should not be identified if missing the three-of-a-kind', () => {
+                expect(new PokerHand('8D 9H 9S QD QH').determineOutcome().rank).not.toBe(outcomes.FULL_HOUSE)
+            });
+        });
+
+        describe('Flush', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('2D 4D 6D 8D TD').determineOutcome().rank).toBe(outcomes.FLUSH)
+            });
+    
+            test('should not be identified if different suits', () => {
+                expect(new PokerHand('2D 4H 6D 8D TD').determineOutcome().rank).not.toBe(outcomes.FLUSH)
+            });
+        });
+
+        describe('Straight', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9H TD JC QS KD').determineOutcome().rank).toBe(outcomes.STRAIGHT)
+            });
+    
+            test('Ace low should be correctly identified', () => {
+                expect(new PokerHand('AH 2D 3C 4S 5D').determineOutcome().rank).toBe(outcomes.STRAIGHT)
+            });
+    
+            test('should not be Straight if not 5 sequential cards', () => {
+                expect(new PokerHand('7H 9D TC JS QD').determineOutcome().rank).not.toBe(outcomes.STRAIGHT)
+            });
+        });
+
+        describe('Three of a Kind', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D 9H 9S 8C AS').determineOutcome().rank).toBe(outcomes.THREE_OF_A_KIND)
+            });
+    
+            test('should not be confused with a Full House', () => {
+                expect(new PokerHand('9D 9S 9H QD QS').determineOutcome().rank).toBe(outcomes.FULL_HOUSE)
+            });
+        });
+
+        describe('Two Pair', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D 9H 8S 8C AS').determineOutcome().rank).toBe(outcomes.TWO_PAIR)
+            });
+    
+            test('should not be identified if incorret cards', () => {
+                expect(new PokerHand('9D 2S 3H 6D 9S').determineOutcome().rank).not.toBe(outcomes.TWO_PAIR)
+            });
+        });
+
+        describe('Pair', () => {
+            test('should be correctly identified', () => {
+                expect(new PokerHand('9D 9H 7S 8C AS').determineOutcome().rank).toBe(outcomes.PAIR)
+            });
+    
+            test('should not be confused with a Full House', () => {
+                expect(new PokerHand('9D 9S 9H QD QS').determineOutcome().rank).toBe(outcomes.FULL_HOUSE)
+            });
+        });
+    });
 });
